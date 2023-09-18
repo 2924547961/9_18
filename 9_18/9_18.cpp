a7cee11d7579986bae570407263c700965a804f5
Xia yihang <2924547961@qq.com>
2023-09-18 21:45:52 +0800

    hi 添加项目文件。

---
+++ 9_18/9_18.cpp
@@ -0,0 +1,52 @@
+//#include <stdio.h>
+//int fun(int a) {
+//	int b = 0;
+//	static int c = 3;
+//	b++;
+//	c++;
+//	return (a + b + c);
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (; i < 3; i++)
+//	
+//		printf("%d", fun(2));
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int getSize(int data[]) {
+//    return sizeof(data);
+//}
+//
+//int main() {
+//    int data1[] = { 1,2,3,4,5 };
+//    int size1 = sizeof(data1);
+//
+//    int* data2 = data1;
+//    int size2 = sizeof(data2);
+//
+//    int size3 = getSize(data1);
+//
+//    printf("%d, %d, %d", size1, size2, size3);
+//    return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	/*int a = 7.0 / 2;
+//	int b = 7 % 3;
+//	printf("%d\n", a);
+//	printf("%d\n", b);*/
+//
+//	float a = 7.0 / 2;	  //除法运算符可以用于整数和浮点数
+//	int b = 7 % 3;        //取模运算符只能用于整数
+//	printf("%2.1f\n", a);
+//	printf("%d\n", b);
+//}
+
+   
---
+++ 9_18/add.cpp
@@ -0,0 +1,5 @@
+//int g_val = 2022;  //全局变量
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
